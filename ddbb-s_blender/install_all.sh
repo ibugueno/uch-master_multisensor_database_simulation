c1287a5e58dbaa0ae4e623b378ff95007546dd90
ibugueno <ignacio.bugueno@live.com>
2024-11-28 16:13:27 -0300

    fixing installers

--- ddbb-s_blender/install_all.sh
+++ ddbb-s_blender/install_all.sh
@@ -6,8 +6,9 @@ TARGET_DIR=~/tmp
 # Create tmp directory if it doesn't exist
 mkdir -p "$TARGET_DIR"
 
-# Install Homebrew
+# Install Homebrew (download and install in tmp)
 echo "Installing Homebrew..."
+cd "$TARGET_DIR"  # Cambiar al directorio tmp
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Update .zprofile for Homebrew (if needed)
@@ -20,8 +21,9 @@ source ~/.zprofile
 echo "Installing tmux..."
 brew install tmux
 
-# Install Blender
+# Install Blender (download and store in tmp)
 echo "Downloading and installing Blender..."
+cd "$TARGET_DIR"  # Asegurarse de que estamos en tmp antes de descargar
 curl -L -O https://download.blender.org/release/Blender4.0/blender-4.0.2-macos-arm64.dmg
 
 # Mount Blender .dmg
