a514e76bafdda720f4fd2ae1aaadef806778b359
ibugueno <ignacio.bugueno@live.com>
2025-06-23 00:31:03 -0400

    adding objects

--- ddbb-s_blender/scripts/1_main_blender_render_back.py
+++ ddbb-s_blender/scripts/1_main_blender_render_back.py
@@ -496,7 +496,22 @@ def main():
     orientations_file = "../objects/orientations_24.txt"
     orientations_degrees = np.loadtxt(orientations_file, skiprows=1)
 
-    object_classes = ['jarron']
+    '''
+    object_classes = [
+        'almohada', 'arbol', 'avion', 'boomerang', 'caja_amarilla', 'caja_azul',
+        'carro_rojo', 'clorox', 'dino', 'disco', 'jarron', 'lysoform', 'mobil',
+        'paleta', 'pelota', 'sombrero', 'tarro', 'tazon', 'toalla_roja', 'zapatilla'
+    ]
+    '''
+    
+    object_classes = [
+        'almohada', 'arbol', 'avion', 'boomerang', 'caja_amarilla', 'caja_azul',
+        'carro_rojo', 'clorox', 'dino', 'jarron', 'lysoform', 'mobil',
+        'paleta', 'pelota', 'tarro', 'sombrero', 'zapatilla'
+    ]
+
+    #object_classes = ['jarron']
+
 
     num_frames = 1500
     height_factor = 1.5
