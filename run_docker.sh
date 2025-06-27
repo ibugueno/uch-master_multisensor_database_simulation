a19dc299de896b9abd51318aa31fe842a5b3642e
ibugueno <ignacio.bugueno@live.com>
2025-06-27 02:31:32 -0400

    fixing shm size

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,10 @@
 
 docker rm -fv ignacio_moving6dpose-s_scripts
 
-docker run -it --gpus '"device=0,1,2,3,4,5,6,7"' --name ignacio_moving6dpose-s_scripts -v /home/ignacio.bugueno/cachefs/datasets/processed_data/moving6dpose/ddbb-s:/app/input -v /home/ignacio.bugueno/cachefs/moving6dpose/models:/app/output ignacio_moving6dpose-s_scripts
+docker run -it \
+  --gpus '"device=0,1,2,3,4,5,6,7"' \
+  --shm-size=64G \
+  --name ignacio_moving6dpose-s_scripts \
+  -v /home/ignacio.bugueno/cachefs/datasets/processed_data/moving6dpose/ddbb-s:/app/input \
+  -v /home/ignacio.bugueno/cachefs/moving6dpose/models:/app/output \
+  ignacio_moving6dpose-s_scripts
