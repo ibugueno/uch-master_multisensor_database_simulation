72f4991dcf88c9250bf550424e4d3191e3917737
ibugueno <ignacio.bugueno@live.com>
2025-06-28 00:18:05 -0400

    fixing recognition 4

--- ddbb-s_dgx1/7_inference_recognition/run_all_rec_training.sh
+++ ddbb-s_dgx1/7_inference_recognition/run_all_rec_training.sh
@@ -15,23 +15,23 @@ for sensor in "asus" "davis346" "evk4"; do
 
     for scene in 0 1 2 3; do
         gpu=$((base_gpu + scene / 2))
-        session=\"eval_rec_${sensor}_scene${scene}\"
-        model_path=\"${MODEL_DIR}${sensor}_scene_${scene}/fasterrcnn_model.pth\"
+        session="eval_rec_${sensor}_scene${scene}"
+        model_path="${MODEL_DIR}${sensor}_scene_${scene}/fasterrcnn_model.pth"
 
-        tmux new-session -d -s $session \"
-            python $SCRIPT_PATH \\\\\\
-                --sensor $sensor \\\\\\
-                --scene $scene \\\\\\
-                --model_path $model_path \\\\\\
-                --input_dir $INPUT_DIR \\\\\\
-                --output_dir $OUTPUT_DIR \\\\\\
-                --gpu $gpu \\\\\\
-                --batch_size $batch_size ; \\\\\\
-            echo '[INFO] Evaluation for $sensor scene $scene finished. Press Enter to close.' ; \\\\\\
+        tmux new-session -d -s $session "
+            python $SCRIPT_PATH \
+                --sensor $sensor \
+                --scene $scene \
+                --model_path $model_path \
+                --input_dir $INPUT_DIR \
+                --output_dir $OUTPUT_DIR \
+                --gpu $gpu \
+                --batch_size $batch_size ; \
+            echo '[INFO] Evaluation for $sensor scene $scene finished. Press Enter to close.' ; \
             read
-        \"
+        "
 
-        echo \"[INFO] Started tmux session '$session' on GPU $gpu for scene $scene with batch_size $batch_size\"
+        echo "[INFO] Started tmux session '$session' on GPU $gpu for scene $scene with batch_size $batch_size"
     done
 done
 
